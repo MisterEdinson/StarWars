8e505b57a07a604952463d985af6f51e331618fc
Edyard Karastoyka <88448179+MisterEdinson@users.noreply.github.com>
2023-07-14 19:39:36 +0500

    add bottom navigation
    add bottom navigation

--- app/src/main/java/com/example/starwars/MainActivity.kt
+++ app/src/main/java/com/example/starwars/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.starwars
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.starwars.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btNav.setupWithNavController(
+            navFragment.findNavController()
+        )
     }
 }
