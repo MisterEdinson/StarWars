944dae01bc1176bb91b9429cc9197d20ee4fe805
Edyard Karastoyka <88448179+MisterEdinson@users.noreply.github.com>
2023-07-15 14:32:07 +0500

    add searching people
    add search
    add font
    add job

--- app/src/main/java/com/example/starwars/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/starwars/ui/home/HomeFragment.kt
@@ -1,9 +1,11 @@
 package com.example.starwars.ui.home
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -11,6 +13,8 @@ import com.example.starwars.R
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
@@ -29,14 +33,27 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initAdapter()
-        viewModel.searchPeople("ob")
         viewModel.searchPeopleData.observe(viewLifecycleOwner, Observer {
             adapter?.list?.submitList(it?.results)
         })
+        etSearchName.addTextChangedListener{
+            search(it)
+        }
     }
 
     private fun initAdapter() {
         adapter = HomeSearchAdapter()
         rvSearchHome.adapter = adapter
+    }
+
+    private fun search(edit:Editable?){
+        job?.cancel()
+        job = MainScope().launch {
+            edit.let {
+                if(it.toString().isNotEmpty() && it.toString().length > 1){
+                    viewModel.searchPeople(it.toString())
+                }
+            }
+        }
     }
 }
